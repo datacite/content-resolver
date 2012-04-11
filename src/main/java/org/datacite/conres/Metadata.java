85fa38999cb2a92a9bb63aad6efa0aa570fc8725
edzuk <ed@21u.co.uk>
2012-04-11 12:44:45 +0100

    parsing optional metadata elements

--- src/main/java/org/datacite/conres/Metadata.java
+++ src/main/java/org/datacite/conres/Metadata.java
@@ -3,12 +3,40 @@ package org.datacite.conres;
 import nu.xom.*;
 
 import javax.ws.rs.core.MediaType;
-import java.io.IOException;
 import java.net.URI;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+
+class Pair {
+    final String key;
+    final String value;
+
+    Pair(String key, String value) {
+        this.key = key;
+        this.value = value;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Pair pair = (Pair) o;
+
+        if (!key.equals(pair.key)) return false;
+        if (!value.equals(pair.value)) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = key.hashCode();
+        result = 31 * result + value.hashCode();
+        return result;
+    }
+}
 
 /**
  * Represents metadata record (parsed from XML)
@@ -20,11 +48,20 @@ public class Metadata {
     private Document document;
     private List<String> creators;
     private String publicationYear;
-    private Map<String, String> titles;
+    private List<Pair> titles;
     private String publisher;
-    private Map<String, String> resourceType;
-    private Map<String, String> descriptions;
-
+    private List<Pair> resourceType;
+    private List<Pair> descriptions;
+    private List<Pair> subjects;
+    private List<String> sizes;
+    private String rights;
+    private List<Pair> dates;
+    private String language;
+    private List<String> formats;
+    private String version;
+    private List<Pair> alternateIdentifiers;
+    private List<Pair> relatedIdentifiers;
+    private List<Pair> contributors;
 
     public Metadata(String doi, String xml, Map<MediaType, URI> media) {
         this.doi = doi;
@@ -42,20 +79,68 @@ public class Metadata {
         this.publicationYear = extractText("publicationYear");
         this.titles = extractPairs("title", "titleType");
         this.publisher = extractText("publisher");
-        this.resourceType = extractPairs("resourceType","resourceTypeGeneral");
+        this.resourceType = extractPairs("resourceType", "resourceTypeGeneral");
         this.descriptions = extractPairs("description", "descriptionType");
+        this.subjects = extractPairs("subject", "subjectScheme");
+        this.sizes = extractList("size");
+        this.rights = extractText("rights");
+        this.dates = extractPairs("date", "dateType");
+        this.language = extractText("language");
+        this.formats = extractList("format");
+        this.version = extractText("version");
+        this.alternateIdentifiers = extractPairs("alternateIdentifier", "alternateIdentifierType");
+        this.relatedIdentifiers = extractRelatedIds();
+        this.contributors = extractContributors();
     }
 
-    private Map<String, String> extractPairs(String elementName, String attributeName) {
-        Map<String, String> result = new HashMap<String, String>();
+    private List<Pair> extractRelatedIds() {
+        Nodes nodes = document.query("//*[local-name() = 'relatedIdentifier']");
+        List<Pair> result = new ArrayList<Pair>();
+        for(int i = 0; i < nodes.size(); i++){
+            Node node = nodes.get(i);
+            Element el = (Element) node;
+            Attribute idType = el.getAttribute("relatedIdentifierType");
+            Attribute relType = el.getAttribute("relationType");
+            String idTypeStr = "";
+            String relTypeStr = "";
+            if (relType != null)
+                relTypeStr = relType.getValue().trim();
+            if (idType != null)
+                idTypeStr = idType.getValue().trim();
+            result.add(new Pair(relTypeStr, idTypeStr.toLowerCase() + ":" + node.getValue().trim()));
+        }
+
+        return result;
+    }
+
+    private List<Pair> extractContributors() {
+        Nodes nodes = document.query("//*[local-name() = 'contributor']");
+        List<Pair> result = new ArrayList<Pair>();
+        for(int i = 0; i < nodes.size(); i++){
+            Node node = nodes.get(i);
+            Element el = (Element) node;
+            Attribute conType = el.getAttribute("contributorType");
+            String conTypeStr = "";
+            if (conType != null)
+                conTypeStr = conType.getValue().trim();
+            result.add(new Pair(conTypeStr, node.getValue().trim()));
+        }
+
+        return result;
+    }
+
+    private List<Pair> extractPairs(String elementName, String attributeName) {
+        List<Pair> result = new ArrayList<Pair>();
         Nodes nodes = document.query("//*[local-name() = '" + elementName + "']");
 
         for(int i = 0; i < nodes.size(); i++){
             Node node = nodes.get(i);
-            String attr = node.getDocument().getRootElement().getAttributeValue(attributeName);
-            if (attr == null)
-                attr = "";
-            result.put(attr, node.getValue().trim());
+            Element el = (Element) node;
+            Attribute attribute = el.getAttribute(attributeName);
+            String attr = "";
+            if (attribute != null)
+                attr = attribute.getValue();
+            result.add(new Pair(attr, node.getValue().trim()));
         }
 
         return result;
@@ -101,7 +186,7 @@ public class Metadata {
         return publicationYear;
     }
 
-    public Map<String, String> getTitles() {
+    public List<Pair> getTitles() {
         return titles;
     }
 
@@ -109,12 +194,52 @@ public class Metadata {
         return publisher;
     }
 
-    public Map<String, String> getResourceType() {
+    public List<Pair> getResourceType() {
         return resourceType;
     }
 
-    public Map<String, String> getDescriptions() {
+    public List<Pair> getDescriptions() {
         return descriptions;
+    }
+
+    public List<Pair> getSubjects() {
+        return subjects;
+    }
+
+    public List<String> getSizes() {
+        return sizes;
+    }
+
+    public String getRights() {
+        return rights;
+    }
+
+    public List<Pair> getDates() {
+        return dates;
+    }
+
+    public String getLanguage() {
+        return language;
+    }
+
+    public List<String> getFormats() {
+        return formats;
+    }
+
+    public String getVersion() {
+        return version;
+    }
+
+    public List<Pair> getAlternateIdentifiers() {
+        return alternateIdentifiers;
+    }
+
+    public List<Pair> getRelatedIdentifiers() {
+        return relatedIdentifiers;
+    }
+
+    public List<Pair> getContributors() {
+        return contributors;
     }
 
     public List<String> getAllMedia(){
--- src/test/java/org/datacite/conres/MetadataTest.java
+++ src/test/java/org/datacite/conres/MetadataTest.java
@@ -30,6 +30,70 @@ public class MetadataTest {
         assertTrue(xml.length() != 0);
         Map<MediaType,URI> media = new HashMap<MediaType, URI>();
         Metadata m = new Metadata(MockSearchServiceImpl.TEST_DOI, xml, media);
+        assertEquals(3, m.getCreators().size());
+        assertEquals(1, m.getTitles().size());
+        assertEquals("Radiolaria abundance of Hole 24-232A", m.getTitles().get(0).value);
+        assertTrue(m.getPublisher().startsWith("PANGAEA"));
+        assertEquals("2005", m.getPublicationYear());
+        assertEquals(32, m.getSubjects().size());
+        assertEquals("Leg24", m.getSubjects().get(29).value);
+        assertEquals("eng", m.getLanguage());
         assertEquals(1, m.getResourceType().size());
+        assertEquals("Dataset", m.getResourceType().get(0).key);
+        assertEquals("Dataset", m.getResourceType().get(0).value);
+        assertEquals("Cites", m.getRelatedIdentifiers().get(0).key);
+        assertEquals("doi:10.2973/dsdp.proc.24.1974", m.getRelatedIdentifiers().get(0).value);
+        assertTrue(m.getSizes().get(0).startsWith("204"));
+        assertEquals("text/tab-separated-values", m.getFormats().get(0));
+    }
+
+    @Test
+    public void parsingTest2(){
+        String xml = loadData("/test2.xml");
+        assertNotNull(xml);
+        assertTrue(xml.length() != 0);
+        Map<MediaType,URI> media = new HashMap<MediaType, URI>();
+        Metadata m = new Metadata(MockSearchServiceImpl.TEST_DOI, xml, media);
+        assertEquals(1, m.getResourceType().size());
+        assertEquals("Text", m.getResourceType().get(0).key);
+        assertEquals("Report", m.getResourceType().get(0).value);
+        assertEquals("IsPartOf", m.getRelatedIdentifiers().get(0).key);
+        assertEquals("doi:10.5284/1000328", m.getRelatedIdentifiers().get(0).value);
+        assertEquals("ADS Grey Lit ID", m.getAlternateIdentifiers().get(0).key);
+        assertEquals("12824", m.getAlternateIdentifiers().get(0).value);
+        assertEquals("OASIS ID", m.getAlternateIdentifiers().get(1).key);
+        assertEquals("universi1-17791", m.getAlternateIdentifiers().get(1).value);
+        assertTrue(m.getSizes().get(0).startsWith("1"));
+        assertEquals("PDF", m.getFormats().get(0));
+        assertTrue(m.getRights().startsWith("http"));
+        assertEquals("Other", m.getDescriptions().get(0).key);
+        assertTrue(m.getDescriptions().get(0).value.startsWith("A4"));
+    }
+
+    @Test
+    public void parsingTest3(){
+        String xml = loadData("/test3.xml");
+        assertNotNull(xml);
+        assertTrue(xml.length() != 0);
+        Map<MediaType,URI> media = new HashMap<MediaType, URI>();
+        Metadata m = new Metadata(MockSearchServiceImpl.TEST_DOI, xml, media);
+        assertTrue(m.getTitles().get(0).value.startsWith("Diabetes"));
+        assertEquals(2, m.getDescriptions().size());
+        assertTrue(m.getDescriptions().get(0).value.length() != 0);
+        assertTrue(m.getDescriptions().get(1).value.length() != 0);
+        assertEquals("Available", m.getDates().get(0).key);
+        assertEquals("2011-11-09", m.getDates().get(0).value);
+    }
+    
+    @Test
+    public void parsingTest4(){
+        String xml = loadData("/test4.xml");
+        assertNotNull(xml);
+        assertTrue(xml.length() != 0);
+        Map<MediaType,URI> media = new HashMap<MediaType, URI>();
+        Metadata m = new Metadata(MockSearchServiceImpl.TEST_DOI, xml, media);
+        assertEquals("HostingInstitution", m.getContributors().get(0).key);
+        assertTrue(m.getContributors().get(0).value.startsWith("Institute"));
+        assertEquals("1.0", m.getVersion());
     }
 }

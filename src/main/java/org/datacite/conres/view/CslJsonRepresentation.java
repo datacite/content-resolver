c2c52411128f64d102d5e6b8379dd50f8ee48197
edzuk <ed@21u.co.uk>
2012-06-28 19:40:56 +0100

    use dataset in CSL JSON

--- src/main/java/org/datacite/conres/view/CslJsonRepresentation.java
+++ src/main/java/org/datacite/conres/view/CslJsonRepresentation.java
@@ -4,6 +4,7 @@ import org.codehaus.jettison.json.JSONArray;
 import org.codehaus.jettison.json.JSONException;
 import org.codehaus.jettison.json.JSONObject;
 import org.datacite.conres.model.Model;
+import org.datacite.conres.model.Pair;
 
 public class CslJsonRepresentation {
     public static JSONObject writeJSON(Model mr) {
@@ -12,8 +13,17 @@ public class CslJsonRepresentation {
             for (String  p : mr.getCreators()) {
                 authors.put(new JSONObject().put("literal", p));
             }
+
+            String type = "misc";
+            for (Pair p : mr.getResourceTypes()){
+                if (p.getKey().toUpperCase().equals("DATASET")){
+                    type = "dataset";
+                    break;
+                }
+            }
+
             return new JSONObject()
-                    .put("type", "misc")
+                    .put("type", type)
                     .put("DOI", mr.getDoi())
                     .put("URL", "http://dx.doi.org/" + mr.getDoi())
                     .put("title", mr.getTitles().get(0).getValue())

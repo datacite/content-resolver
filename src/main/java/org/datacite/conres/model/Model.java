bd72362009cb0ad00275c1e82bd637fdb55b8484
Martin Fenner <martin.fenner@datacite.org>
2016-08-22 22:09:31 +0200

    escape characters according to latex in bibtex output format. Closes #2

--- src/main/java/org/datacite/conres/model/Model.java
+++ src/main/java/org/datacite/conres/model/Model.java
@@ -299,4 +299,93 @@ public class Model {
     public Date getUploaded() {
         return uploaded;
     }
+
+    public static final String escapeLatex(String text) {
+        StringBuilder sb=new StringBuilder(text.length());
+        boolean nl=false;
+        for (int i=0,c=text.length(); i<c; i++) {
+            char ch=text.charAt(i);
+            if (ch!=13 && ch!=10 && nl) {
+                sb.append("\\\\\n");
+                nl=false;
+            }
+            switch (ch) {
+                case '\u00E4': sb.append("{\\\"a}"); break;
+                case '\u00F6': sb.append("{\\\"o}"); break;
+                case '\u00FC': sb.append("{\\\"u}"); break;
+                case '\u00EB': sb.append("{\\\"e}"); break;
+                case '\u00EF': sb.append("{\\\"i}"); break;
+
+                case 196: sb.append("{\\\"A}"); break;
+                case 214: sb.append("{\\\"O}"); break;
+                case 220: sb.append("{\\\"U}"); break;
+                case 203: sb.append("{\\\"E}"); break;
+                case 207: sb.append("{\\\"I}"); break;
+
+                case 225: sb.append("{\\'a}"); break;
+                case 243: sb.append("{\\'o}"); break;
+                case 250: sb.append("{\\'u}"); break;
+                case 233: sb.append("{\\'e}"); break;
+                case 237: sb.append("{\\'i}"); break;
+
+                case 224: sb.append("{\\`a}"); break;
+                case 242: sb.append("{\\`o}"); break;
+                case 249: sb.append("{\\`u}"); break;
+                case 232: sb.append("{\\`e}"); break;
+                case 236: sb.append("{\\`i}"); break;
+
+                case 226: sb.append("{\\^a}"); break;
+                case 244: sb.append("{\\^o}"); break;
+                case 251: sb.append("{\\^u}"); break;
+                case 234: sb.append("{\\^e}"); break;
+                case 238: sb.append("{\\^i}"); break;
+
+                case 194: sb.append("{\\^A}"); break;
+                case 212: sb.append("{\\^O}"); break;
+                case 219: sb.append("{\\^U}"); break;
+                case 202: sb.append("{\\^E}"); break;
+                case 206: sb.append("{\\^I}"); break;
+
+                case 227: sb.append("{\\~a}"); break;
+                case 241: sb.append("{\\~n}"); break;
+                case 245: sb.append("{\\~o}"); break;
+
+                case 195: sb.append("{\\~A}"); break;
+                case 209: sb.append("{\\~N}"); break;
+                case 213: sb.append("{\\~O}"); break;
+
+                case '\u00DF': sb.append("{\\ss}"); break;
+                case '\u00A0': sb.append('~'); break; // &nbsp;
+                case '\u00BA': sb.append("{\\textdegree}"); break;
+                case '"': sb.append("{\"}"); break;
+
+                case 13:
+                case 10:
+                    nl=true;
+                    break;
+
+                case '\'':
+                case '\u00B4':
+                case '`':
+                    sb.append("{\'}"); break;
+
+                // simple escapes:
+                case '\\':
+                case '~':
+                case '$':
+                case '%':
+                case '^':
+                case '&':
+                case '{':
+                case '}':
+                case '_':
+                    sb.append('\\');
+                    sb.append(ch);
+                    break;
+                default:
+                    sb.append( (ch<0x80)?ch:'?' );
+            }
+        }
+        return sb.toString();
+    }
 }
--- src/main/resources/freemarker/doi_bibtex.ftl
+++ src/main/resources/freemarker/doi_bibtex.ftl
@@ -1,8 +1,9 @@
 @misc{https://doi.org/${doi},
   doi = {${doi}},
   url = {https://doi.org/${doi}},
-  author = {<#list creators! as c>${c}<#sep> and </#list>},
-  publisher = {${publisher!}},
-  title = {${titles[0].value}},
-  year = {${publicationYear!}}
+  author = {<#list creators! as c>${escapeLatex(c)}<#sep> and </#list>},
+  publisher = {${escapeLatex(publisher)!}},
+  title = {${escapeLatex(titles[0].value)}},
+  year = {${publicationYear!}},
+  abstract={${escapeLatex(descriptions[0].value)}}
 }
